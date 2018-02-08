b37f649ef2aba50f78b3d67395d02c40e3f624bc
Zukamimozu <xjacobhuber@gmail.com>
2018-02-07 17:43:43 -0700

    Add files via upload

--- Game.java
+++ Game.java
@@ -2,6 +2,8 @@ public class Game {
 	public final static int gridWidth = 8;
 	public final static int gridHeight = 24;
 
+	private Block[] arrayBlocks = new Block[8*24];
+
 
 	// 2D Array of grid spots
 	// 1D Array of all created blocks
@@ -12,9 +14,6 @@ public class Game {
 	private int score = 0;
 	private int time = 0;
 
-	public Game(){
-	}
-
 
 	public void createBlock() {
 		// Create a new block at the top centre of the screen
@@ -23,5 +22,25 @@ public class Game {
 
 	public void tick() {
 		// Get user input, and move block down 1 space
+	}
+
+	public Block[] getArrayBlocks() {
+		return this.arrayBlocks;
+	}
+
+	public void printScreen() {
+		String screen = "";
+		for (int col = 0; col < game.gridHeight; col++) {
+			for (int row = 0; row <  game.gridWidth; row++) {
+				
+				for (Block selectedBlock : game.getArrayBlocks()) {
+					if (col == selectedBlock.getPositionX() && row == selectedBlock.getPositionY()) {
+						screen
+					}
+				}
+
+			}
+		}
+
 	}
 }
