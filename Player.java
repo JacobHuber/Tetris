666ff8d1ac76385257b6b018a643d1b1c92a63dc
EvanLZQ <36173749+EvanLZQ@users.noreply.github.com>
2018-02-07 17:34:34 -0800

    Add files via upload

--- Player.java
+++ Player.java
@@ -1,9 +1,38 @@
-public class Player {
+import java.util.Scanner;
+
+public class Player 
+{
 	Block blockFalling;
 	Block blockHold;
-
-
-	public void getUserInput() {
+	Scanner read = new Scanner(System.in);
+	
+	public void getUserInput() 
+	{
+		
+		
+		System.out.println("Please enter how you want to move the block: ");
+		
+		String move = read.nextLine();
+		
+		read.close();
+		
+		move = move.toUpperCase();
+		
+		switch(move)
+		{
+		case "LEFT":
+			blockFalling.moveLeft();
+			break;
+		case "RIGHT":
+			blockFalling.moveRight();
+		    break;
+		case "SET":
+			blockFalling.placeBlock();
+			break;
+		default:
+			System.out.println("Please enter a valid move.");
+			break;
+		}
 		// Get input, left, right, or set, respond accordingly
 	}
 }
