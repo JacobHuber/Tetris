49e43c06e3fad080418e50bac07910b7be607ab5
Zukamimozu <xjacobhuber@gmail.com>
2018-02-09 12:29:09 -0700

    Update Tetris.java

--- Tetris.java
+++ Tetris.java
@@ -1,15 +1,19 @@
-//Main class. Runs all the necessary methods for the game.
-
+/**
+ * Main class. Runs all the necessary components for the game.
+ */
 public class Tetris {
-    //Creates a new game using methods from the "Game" class
+    /** 
+     * Creates a new game and runs it.
+     */
     public static void main(String[] args) {
         Game game = new Game();
         
-        //Runs the game
+        // Runs the game.
         while (game.getGameRunning()) {
             game.tick();
         
-        }//If the game isnt running or the endgame condition is met, it will print "Game Over!"
+        }
+        // If the game isn't running or the endgame condition is met, it will print "Game Over!".
         System.out.println("Game Over!");
 
     }
