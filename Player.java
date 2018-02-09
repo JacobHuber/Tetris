c2691f2b28cf71597b99f23bb80ff6a290a5c74d
Zukamimozu <xjacobhuber@gmail.com>
2018-02-09 12:44:48 -0700

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,24 +1,25 @@
 import java.util.Scanner;
 
 public class Player 
-{	//Setting the blocks. One will be for the block falling and the latter is for the blocks that will be held.(blockHold is WIP)
+{	// The current falling block.
 	Block blockFalling;
+	// The block that is stored/held (This feature has yet to be implemented).
 	Block blockHold;
 	
-	//Scanner will be used to read the user input.
+	// Scanner will be used to read the user input.
 	Scanner read = new Scanner(System.in);
 	
 	public void getUserInput() 
 	{
 		
-		//Getting the user's input and reading it.
-		System.out.println("Please enter how you want to move the block: ");
+		// Prompts the user for their input.
+		System.out.println("Please enter how you want to move the block ('LEFT', 'RIGHT', or 'SET' it in place): ");
 		String move = read.nextLine();
 		
 		//Converting the user's input to uppercase to prevent complications.
 		move = move.toUpperCase();
 		
-		// Get input, left, right, or set, respond accordingly
+		// Do the user's input move (Or prompt them to input a proper move.)
 		switch(move)
 		{
 		case "LEFT":
@@ -31,7 +32,7 @@ public class Player
 			blockFalling.placeBlock();
 			break;
 		
-		//Default case in case the user's input dosent satisfy any of the cases.
+		// Default case in case the user's input dosent satisfy any of the cases.
 		default:
 			System.out.println("Please enter a valid move.");
 			break;
