85960d6ac06f1044773d70b3c7bbf661e42e5ed8
LighTec <ktech117@gmail.com>
2018-02-09 12:42:13 -0700

    Added comments to GUI code

--- MainViewFX.java
+++ MainViewFX.java
@@ -14,17 +14,24 @@ import javafx.stage.Stage;
  */
 public class MainViewFX extends Application {
 
+    // Specific panes
     private FlowPane tetrisPane;
     private FlowPane colorPane;
 
+    // The rectangles representing the Tetris grid
     private Rectangle[][] rectangles;
 
+    // Pre-set color codes for easy modification
     private final String hexHbox = "4A444B";
     private final String hexButtonBox = "BA0101";
     private final String hexColorBox = "FFFFF0";
     private final String hexDelayBox = "78866B";
     private final String hexSliderBox = "90AFFF";
 
+    /**
+     * Launches the GUI window.
+     * @param args 
+     */
     public static void init(String[] args) {
         launch(args);
     }
@@ -36,14 +43,18 @@ public class MainViewFX extends Application {
     public void start(Stage primaryStage) {
         primaryStage.setTitle("Tetris V01");
 
+        // The root pane, has a basic layout allowing other panes to go on top
         BorderPane root = new BorderPane();
 
-        root.setRight(addScorePane());
+        // Sets panes on top of the root pane
+        root.setRight(addScorePane(null));
         root.setLeft(addNextBlockPane());
         root.setCenter(addTetrisPane());
 
+        // Creates a scene, which is what is actually displayed. Uses the root pane.
         Scene scene = new Scene(root);
 
+        // Sets the scene, and shows it to the user.
         primaryStage.setScene(scene);
         primaryStage.show();
     }
@@ -52,18 +63,26 @@ public class MainViewFX extends Application {
     * Generates the flowPane acting as the tetris window
     */
     private FlowPane addTetrisPane() {
+        // Creates a new, empty pane
         FlowPane tetrisPane = new FlowPane();
+        // Sets the packground color
         tetrisPane.setStyle("-fx-background-color: #" + this.hexColorBox + ";");
+        
+        // Sets the spacing for the Rectangle objects within
         tetrisPane.setPadding(new Insets(5, 10, 5, 10));
         tetrisPane.setVgap(4);
         tetrisPane.setHgap(4);
         tetrisPane.setPrefWrapLength(900); // preferred width allows for two columns
         
+        
+        // Temp code, for testing. Generates 11 rectangles that should autofill the tetris grid, or to test it anyways.
             Rectangle rect1 = new Rectangle(75, 75);
             rect1.setFill(Color.CORNFLOWERBLUE);
             Rectangle rect2 = new Rectangle(75, 75);
             rect2.setFill(Color.GREENYELLOW);
+            // Creates new rectangle, 75 by 75 pixel size.
             Rectangle rect3 = new Rectangle(75, 75);
+            // Sets the rectangle a certain color.
             rect3.setFill(Color.DARKVIOLET);
             Rectangle rect4 = new Rectangle(75, 75);
             rect4.setFill(Color.BURLYWOOD);
@@ -87,11 +106,23 @@ public class MainViewFX extends Application {
         return tetrisPane;
     }
     
+    /**
+     *  Generates a pane for displaying the next 3 tetronimos 
+     * @return Vbox pane 
+     */
     private VBox addNextBlockPane(){
         return new VBox();
     }
     
-    private FlowPane addScorePane(){
+    /**
+     * Generates a pane with a scoreboard for high scores, displaying top 10, and current score.
+     * Can either be given null, for no saved high scores, or given a file location as a string to load scores from.
+     * @return Flowpane pane
+     */
+    private FlowPane addScorePane(String highScoreFile){
+        if(highScoreFile != null){
+            
+        }
         return new FlowPane();
     }
 
